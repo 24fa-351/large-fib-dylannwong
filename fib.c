c82794ad5544336dd8f3d81c1a03255c6a7d5fa9
dylannwong <dylannwong@csu.fullerton.edu>
2024-09-18 18:52:16 -0700

    long fib

--- fib.c
+++ fib.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[]) {
-   printf("Hello!\n");
+unsigned long long fibbi (int n) {
+
+   unsigned long long num1 = 0;
+   unsigned long long num2 = 1;
+   unsigned long long sum = 1;
+
+   if(n == 1) {
+      return 0;
+   } else {
+      for(int i = 0; i < n - 1; ++i) {
+         if(i + 2 == n) {
+            return sum;
+         }
+         sum = num1 + num2;
+         num1 = num2;
+         num2 = sum;
+      }
+   }
+   
+}
+
+unsigned long long fibbr(int n) {
+
+   if(n == -1) {
+      return 0;
+   }
+   if (n == 0 || n == 1) {
+      return 1;
+   }
+
+   return fibbr(n - 1) + fibbr(n - 2);
+}
+
+int main(int argc, char *argv[]) {
+
+   int n = atoi(argv[1]);
+
+   if(*argv[2]=='i') {
+      printf("%lld", fibbi(n));
+   } else if(*argv[2]=='r') {
+      printf("%lld", fibbr(n-2));
+   }
+   
    return 0;
 }
