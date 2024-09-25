67a5e3118160d855dfda3bb400d23f17037dc6c7
dylannwong <dylannwong@csu.fullerton.edu>
2024-09-24 23:38:28 -0700

    clean long fib

--- fib.c
+++ fib.c
@@ -1,48 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned long long fibbi (int n) {
+typedef unsigned long long unlong;
 
-   unsigned long long num1 = 0;
-   unsigned long long num2 = 1;
-   unsigned long long sum = 1;
+unlong fibbi(int n) {
 
-   if(n == 1) {
-      return 0;
-   } else {
-      for(int i = 0; i < n - 1; ++i) {
-         if(i + 2 == n) {
-            return sum;
-         }
-         sum = num1 + num2;
-         num1 = num2;
-         num2 = sum;
-      }
-   }
-   
+  unlong num1 = 0;
+  unlong num2 = 1;
+  unlong sum;
+  if (n <= 1) {
+    return 0;
+  } else {
+    for (int ix = 1; ix < n - 1; ++ix) {
+      sum = num1 + num2;
+      num1 = num2;
+      num2 = sum;
+    }
+    return sum;
+  }
 }
 
-unsigned long long fibbr(int n) {
+unlong fibbr(int n) {
 
-   if(n == -1) {
-      return 0;
-   }
-   if (n == 0 || n == 1) {
-      return 1;
-   }
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  }
 
-   return fibbr(n - 1) + fibbr(n - 2);
+  return fibbr(n - 1) + fibbr(n - 2);
 }
 
 int main(int argc, char *argv[]) {
 
-   int n = atoi(argv[1]);
+  int n = atoi(argv[1]);
 
-   if(*argv[2]=='i') {
-      printf("%lld", fibbi(n));
-   } else if(*argv[2]=='r') {
-      printf("%lld", fibbr(n-2));
-   }
-   
-   return 0;
+  if (*argv[2] == 'i') {
+    printf("%lld", fibbi(n));
+  } else if (*argv[2] == 'r') {
+    printf("%lld", fibbr(n - 1));
+  }
+
+  return 0;
 }
